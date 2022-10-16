a278a3be1f98be131a533ae7beb5e380970c046b
Effram Partidas <efframlol@gmail.com>
2022-10-16 09:30:07 -0500

    You can say Hi to yourself

---
+++ Display_user_name/script.js
@@ -0,0 +1,5 @@
+const U_name = document.getElementById("name")
+const button = document.querySelector("button")
+const U_input = document.querySelector("input")
+
+button.onclick = () => U_name.innerHTML = (U_input.value)
