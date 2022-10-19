b3021eef9d6963f34e61ceb6cbc036e48a22778e
Effram Partidas <efframlol@gmail.com>
2022-10-19 12:30:42 -0500

    Reset the challenge

--- 99_overflow_problems/menu.js
+++ 99_overflow_problems/menu.js
@@ -1,1 +1,9 @@
-const list = document.getElementsByClassName("li")
+const btn = document.querySelector("button")
+const menu_list = document.querySelector("ul")
+const menu = document.querySelector("nav")
+
+btn.onclick = () => {
+    menu_list.classList.remove("hide")
+    menu.classList.add("close")
+    btn.classList.add("close_btn")
+}
