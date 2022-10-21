86412c75b8a868a63bcca572a8f78833a9b5cde6
Effram Partidas <efframlol@gmail.com>
2022-10-21 11:45:51 -0500

    Made some progress, but still need work

--- Hexmaker/script.js
+++ Hexmaker/script.js
@@ -1,3 +1,19 @@
 // Variables
+const hex_values_array = ["0", "1", "2", "3", "4", "5", "6", "A", "B", "C", "D", "E", "F"]
+const val_1 = document.getElementById("val_1")
+const val_2 = document.getElementById("val_2")
+const val_3 = document.getElementById("val_3")
+const val_4 = document.getElementById("val_4")
+const val_5 = document.getElementById("val_5")
+const val_6 = document.getElementById("val_6")
+const hex_color = document.getElementById("hex_color")
+let hex_color_value = "#" + val_1
 
 // Funtions
+
+val_1.innerText = hex_values_array[12]
+val_2.innerText = hex_values_array[12]
+val_3.innerText = hex_values_array[12]
+val_4.innerText = hex_values_array[12]
+val_5.innerText = hex_values_array[12]
+val_6.innerText = hex_values_array[12]
